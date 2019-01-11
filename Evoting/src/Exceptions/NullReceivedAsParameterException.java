9efe99f69a73f22cd03bd529ef665ebac77e1c36
Albert Planes <apseuma@gmail.com>
2019-01-12 00:18:09 +0100

    Exception created

---
+++ Evoting/src/Exceptions/NullReceivedAsParameterException.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+public class NullReceivedAsParameterException extends Exception {
+
+    public NullReceivedAsParameterException(String msg) {
+        super(msg);
+    }
+
+}
