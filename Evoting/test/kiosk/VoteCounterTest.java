e71fcf36c8a5404d3c2b2e1d05cffa3ab7b8e812
mikiribe14 <alcarras.miquel.ribes@gmail.com>
2019-01-12 01:56:23 +0100

    CorrectScruitize() en Test de VoteCounter

--- Evoting/test/kiosk/VoteCounterTest.java
+++ Evoting/test/kiosk/VoteCounterTest.java
@@ -3,19 +3,21 @@ package kiosk;
 
 import Exceptions.NullReceivedAsParameterException;
 import data.Party;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VoteCounterTest {
 
     private VoteCounter counter;
     Set<Party> votables;
 
-    @BeforeEach
+    @BeforeAll
     public void SetUp() throws NullReceivedAsParameterException {
         votables= new HashSet<>(Arrays.asList(new Party ("PP"),new Party ("PSOE"),
                   new Party ("Podemos"),new Party ("Cs"),new Party ("ERC")));
@@ -25,11 +27,28 @@ public class VoteCounterTest {
 
     @Test
     void InitCounterToZero() throws NullReceivedAsParameterException{
-        Assert.equals(0,counter.getVotesFor(new Party("PP")));
-        Assert.equals(0,counter.getVotesFor(new Party("ERC")));
-        Assert.equals(0,counter.getVotesFor(new Party("Podemos")));
-        Assert.equals(0,counter.getBlanks());
-        Assert.equals(0,counter.getNulls());
+        assertEquals(0,counter.getVotesFor(new Party("PP")));
+        assertEquals(0,counter.getVotesFor(new Party("ERC")));
+        assertEquals(0,counter.getVotesFor(new Party("Podemos")));
+        assertEquals(0,counter.getBlanks());
+        assertEquals(0,counter.getNulls());
+
+    }
+
+    @Test
+    void CorrectScrutinize() throws NullReceivedAsParameterException {
+        counter.scrutinize(new Party("ERC"));
+        counter.scrutinize(new Party("PP"));
+        counter.scrutinize(new Party("PP"));
+        counter.scrutinize(new Party("partit inventat"));
+        counter.scrutinize(new Party("un altre partit inventat"));
+        counter.scrutinize(new Party("BLANK"));
+
+        assertEquals(1,counter.getVotesFor(new Party("ERC")));
+        assertEquals(2,counter.getVotesFor(new Party("PP")));
+        assertEquals(2,counter.getNulls());
+        assertEquals(1,counter.getBlanks());
+
 
     }
 }
